16763600e8f9fdc39bb5dea8f36386a6d34cdcbe
Diskettencoder <157545132+Diskettencoder@users.noreply.github.com>
2026-05-11 10:53:01 +0200

    bingo.js aktualisieren

--- bingo.js
+++ bingo.js
@@ -23,7 +23,7 @@ const sins = [
     "Greets you with ”WE NEED TO TALK”",
     "Buzzword: Not a Company",
     "Repeats the word ”Method” without elaborating",
-    "Joker (recommendations welcome)",
+    "”We work with Data.”",
     "Joker (recommendations welcome)",
     "Joker (recommendations welcome)",
     "Joker (recommendations welcome)"
